08892e38bb0cefa587d0e3616317924c87352b59
Ghiuta Beatrice <ghiutaanamaria@yahoo.com>
2019-03-21 13:32:44 +0200

    Add Book model class

---
+++ OkReads/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OkReads.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        public string Isbn { get; set; }
+        public float Score { get; set; }
+        public string CoverPath { get; set; }
+
+        public virtual Genre Genre { get; set; }
+        public virtual ICollection<Author> Authors { get; set; }
+
+        public Book() { }
+        public Book(string title, string isbn, string coverPath, Genre genre, ICollection<Author> author)
+        {
+            this.Title = title;
+            this.Isbn = isbn;
+            this.CoverPath = coverPath;
+            this.Genre = genre;
+            this.Authors = author;
+        }
+    }
+}
