67f06d6535a91628a829d3922b174f03c4dad682
Mihalache Bogdan <bogdanmihalache97@gmail.com>
2019-03-24 19:35:03 +0200

    Add LibraryEntry model class

---
+++ OkReads/Models/LibraryEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OkReads.Models
+{
+    public enum BookStatus { ToRead, Reading, Read }
+
+    public class LibraryEntry
+    {
+        public int LibraryEntryId { get; set; }
+        public BookStatus BookStatus { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public virtual Book Book { get; set; }
+
+        public LibraryEntry() { }
+
+        public LibraryEntry(ApplicationUser user, Book book, BookStatus status)
+        {
+            User = user;
+            Book = book;
+            BookStatus = status;
+        }
+    }
+}
