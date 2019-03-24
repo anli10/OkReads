6e23786c2c2bc5aaf7b3f89bb28426fb5f3a8d54
Mihalache Bogdan <bogdanmihalache97@gmail.com>
2019-03-24 19:23:12 +0200

    Add Genre model class

---
+++ OkReads/Models/Genre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OkReads.Models
+{
+    public class Genre : IEquatable<Genre>
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+
+        virtual public ICollection<Book> Books { get; set; }
+
+        public Genre() { }
+        
+        public Genre(string name)
+        {
+            this.Name = name;
+        }
+
+        public bool Equals(Genre other)
+        {
+            return this.Name.Equals(other.Name);
+        }
+    }
+}
