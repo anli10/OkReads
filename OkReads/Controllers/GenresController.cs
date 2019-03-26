54dca821c1e047de5a4f3bcabc4b9cf2b5d45a43
Belu Marian <belu.marian97@yahoo.com>
2019-03-26 14:43:57 +0200

    Add Details action for genre controller

--- OkReads/Controllers/GenresController.cs
+++ OkReads/Controllers/GenresController.cs
@@ -20,6 +20,23 @@ namespace OkReads.Controllers
             return View(db.Genres.ToList());
         }
 
+        // GET: Genres/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Genre = genre;
+            ICollection<Book> books = db.Books.Where(x => x.Genre.Name == genre.Name).ToList();
+            return View(books);
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
