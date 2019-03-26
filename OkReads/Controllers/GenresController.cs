0bbbbdd1fbec78ec69388dac59c2a57e80fb8ed4
Belu Marian <belu.marian97@yahoo.com>
2019-03-26 14:28:07 +0200

    Add Genre controller class

---
+++ OkReads/Controllers/GenresController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OkReads.Models;
+
+namespace OkReads.Controllers
+{
+    public class GenresController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Genres
+        public ActionResult Index()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
